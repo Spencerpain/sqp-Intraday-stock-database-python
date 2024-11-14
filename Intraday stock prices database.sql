dc6bd3ef7783238016f90a72b0a52111a3a7e79f
Spencer <spencerpain26@gmail.com>
2024-11-13 22:41:01 -0500

    Intraday stock prices database.sql

--- Intraday stock prices database.sql
+++ Intraday stock prices database.sql
@@ -1,1 +1,17 @@
-SELECT * FROM intraday_trading.intraday_prices;
+-- setup.sql
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS intraday_trading;
+USE intraday_trading;
+
+-- Create table for storing intraday stock prices
+CREATE TABLE IF NOT EXISTS intraday_prices (
+    ticker VARCHAR(10) NOT NULL,
+    timestamp DATETIME NOT NULL,
+    open FLOAT,
+    high FLOAT,
+    low FLOAT,
+    close FLOAT,
+    volume INT,
+    PRIMARY KEY (ticker, timestamp)
+);
